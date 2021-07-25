687fe96d0c9ee736dadc73d4cd71611ccbf8b96b
Dhruv Bansal <bansaldhruv1973@gmail.com>
2021-07-25 15:45:46 +0530

    :speak_no_evil:

---
+++ Sorting/Insertion_sort.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+    	for i in range(1, len(arr)):
+		    key = arr[i] # this element will be now inserted at it's correct position
+		    j = i - 1 # because the right place of insertion will be on left
+		    while j >= 0 and key < arr[j]:
+			    arr[j+1] = arr[j]
+			    j -= 1
+		    arr[j+1] = key
+		
+	return
+# >> Time complexity:     O(n^2)
+# >> Space complexity:    O(1)
+# >> In-place:            YES
+# >> No. of comparisons:  O(n^2)
+# >> No. of swaps:        O(n^2)
+# >> Stability:           STABLE
+# 
+# >> Time complexity in best case:       Omega(n)
+# >> Time complexity in avg case:        Theta(n^2)
